9dc9108f85ebab640b586f9ca8d897966a60a88b
CryptoNube <daverad@hotmail.com>
2023-04-18 00:44:11 -0500

    new contract

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x6bFd368BB305fc49e0de66CFbAde43279c622Cdc";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-eta-kohl.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
